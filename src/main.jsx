353112dfe2a218c2dbe83633bb3294a1f1478f9c
ginaapaola <ginapaolamc07@gmail.com>
2024-04-18 19:48:29 -0500

    creacion de game-card

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,30 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { GameCard } from "./gameCard"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <GameCard
+      image="https://image.api.playstation.com/cdn/UP0006/CUSA01925_00/RxeNb9Ph1y2VhBGv5Ct0tuY6f5xC4t9f.png"
+      gameTitle="NEED FOR SPEED: DELUXE EDITION"
+      gameUrl="#"
+      descriptionGame="juego #1"
+    />
+
+    <GameCard
+    image="https://www.irrompibles.net/irrwp/wp-content/uploads/2019/11/NeedForSpeedHeat-head.jpg"
+    gameTitle="NEED FOR SPEED: HEAT"
+    gameUrl="#"
+    descriptionGame="juego #2"
+    />
+
+<GameCard
+    image="https://cdn.akamai.steamstatic.com/steam/apps/1262560/header.jpg?t=1605151411"
+    gameTitle="NEED FOR SPEED: MOST WANTED"
+    gameUrl="#"
+    descriptionGame="juego #3"
+    />
+
   </React.StrictMode>,
 )
