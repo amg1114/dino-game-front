5a4ac4a5da7a276c6bba1d483cbee873e0e9dbb0
1Nath00 <nataliagomezdelacruz1424@gmail.com>
2024-04-26 23:16:30 -0500

    creacion rama develop/noticias-page

--- src/pages/Noticias/PaginaNoticias/PaginaNoticias.jsx
+++ src/pages/Noticias/PaginaNoticias/PaginaNoticias.jsx
@@ -1,5 +1,24 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 
 export function PaginaNoticias() {
-    return <> <h1>Pagina Noticias</h1> <Outlet /> </>
+    const ENDPOINT_API = "https://dino-game-backend-production.up.railway.app/api/noticias"
+    const [noticias, setNoticias] = useState([]);
+
+    useEffect(() => { 
+        axios.get(ENDPOINT_API)
+            .catch(function (error) {
+                console.log(error)
+            })
+            .then(function (respuesta) {
+                setNoticias(respuesta.data)
+            })
+        
+    })
+    console.log (noticias)
+
+    return <>
+        <Outlet />
+    </>
 }
