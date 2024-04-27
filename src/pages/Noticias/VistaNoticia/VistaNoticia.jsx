52bc0a86a938c5b91dd20f5f1aaa9bf79ddb82a8
Alejandro Moreno <alejandromogil@gmail.com>
2024-04-26 21:27:37 -0500

    Remove Vista noticia content

--- src/pages/Noticias/VistaNoticia/VistaNoticia.jsx
+++ src/pages/Noticias/VistaNoticia/VistaNoticia.jsx
@@ -1,19 +1,3 @@
 export function VistaNoticia() {
-    return <>
-        <div className="modal-fade animate__animated animate__fadeIn">
-            <div className="modal-content animate__animated animate__slideInDown">
-                <div className="modal-header">
-                    <Link to="/juegos" className="modal-closer">
-                        <span className="material-symbols-outlined">
-                            close
-                        </span>
-                    </Link>
-                </div>
-               <div className="container">
-
-                                    
-               </div>
-            </div>
-        </div>
-    </>
+    return <h1>VistaNoticia</h1>
 }
