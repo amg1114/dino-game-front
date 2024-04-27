364c31a6abb60450082ea7a773cb8c23629d8ec6
Alejandro Moreno <alejandromogil@gmail.com>
2024-04-26 21:15:57 -0500

    Modal design

--- src/pages/Juegos/VistaJuego/VistaJuego.jsx
+++ src/pages/Juegos/VistaJuego/VistaJuego.jsx
@@ -1,3 +1,18 @@
+import { Link } from "react-router-dom";
+
 export function VistaJuego() {
-    return <h1>Vista Juego</h1>
+    return (<>
+        <div className="modal-fade animate__animated animate__fadeIn">
+            <div className="modal-content animate__animated animate__slideInDown">
+                <div className="modal-header">
+                    <Link to="/juegos" className="modal-closer">
+                        <span className="material-symbols-outlined">
+                            close
+                        </span>
+                    </Link>
+                </div>
+                <h1>Titulo del Juego</h1>
+            </div>
+        </div>
+    </>)
 }
