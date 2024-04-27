fd3b0c66b51a40120e8e7531721adb6d583c1541
Felipe Cabal <phelipekabbal_85@hotmail.com>
2024-04-27 15:58:36 -0500

    solucion del merge

--- src/components/CarouselComponent/CarouselComponent.jsx
+++ src/components/CarouselComponent/CarouselComponent.jsx
@@ -42,7 +42,6 @@ export const CarouselComponent = ({ slides }) => {
   }
 
   return <>
-<<<<<<<<< Temporary merge branch 1
     {slides.length ? (
       <div className="carousel">
         <div className="imgWrapper">
@@ -76,68 +75,4 @@ export const CarouselComponent = ({ slides }) => {
       </div>
     ) : <></>}
   </>;
-=========
-    {
-      slides.length > 0 ? (
-
-        <div className="carousel">
-
-          <div className="imgWrapper">
-            <img src={slides[currentSlide].url} alt={slides[currentSlide].title} />
-            <div className="controls">
-              <button onClick={prevSlide} className="prev-button" type="button"> <span className="material-symbols-outlined">
-                arrow_back_ios_new
-              </span> </button>
-              <button onClick={nextSlide} className="next-button" type="button"> <span className="material-symbols-outlined">
-                arrow_forward_ios
-              </span> </button>
-            </div>
-          </div>
-
-        <div className="selectores">
-          {
-            slides.map(
-              (slide, index) => {
-                return (
-                  <button className={"selector " + (currentSlide === index ? 'active' : '')} key={'slide' + index} onClick={() => (selectSlide(index))} type="button">
-                  </button>
-                )
-              }
-            )
-          }
-        </div>
-      </div>
-    ) : <></>}
-  </>;
-          <div className="selectores">
-            {
-              slides.map(
-                (slide, index) => {
-                  return (
-                    <button className={"selector " + (currentSlide === index ? 'active' : '')} key={'slide' + index} onClick={() => (selectSlide(index))} type="button">
-                    </button>
-                  )
-                }
-              )
-            }
-          </div>
-        </div>
-      ) : <></>
-    }
-  </>
-
-
-
-
-    /*<div className="slider">
-      <div className="slide">
-        <img src={slides[currentSlide].imageUrl} alt={slides[currentSlide].altText} />
-        <h2>{slides[currentSlide].title}</h2>
-        <p>{slides[currentSlide].description}</p>
-      </div>
-      <button onClick={prevSlide}>Previous</button>
-      <button onClick={nextSlide}>Next</button>
-</div>*/
-
 }
-
