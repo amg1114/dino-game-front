faa07e1129df0e96721c7f4ed0c62c6011b4f302
1Nath00 <nataliagomezdelacruz1424@gmail.com>
2024-04-27 12:25:31 -0500

    commit 2

--- src/pages/Noticias/PaginaNoticias/PaginaNoticias.jsx
+++ src/pages/Noticias/PaginaNoticias/PaginaNoticias.jsx
@@ -1,12 +1,13 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
+import { CardNotice } from "../../../components/Noticias/CardNoticia";
 
 export function PaginaNoticias() {
     const ENDPOINT_API = "https://dino-game-backend-production.up.railway.app/api/noticias"
     const [noticias, setNoticias] = useState([]);
 
-    useEffect(() => { 
+    useEffect(() => {
         axios.get(ENDPOINT_API)
             .catch(function (error) {
                 console.log(error)
@@ -14,11 +15,33 @@ export function PaginaNoticias() {
             .then(function (respuesta) {
                 setNoticias(respuesta.data)
             })
-        
+
     })
-    console.log (noticias)
 
     return <>
-        <Outlet />
+        {noticias === null ? <></> : (
+            <div>
+                <ul>
+                    {noticias.map((elemento, index) => {
+                        return (
+
+                            <li>
+                                    <CardNotice
+                                        image={elemento.assets[0].url}
+                                        title={elemento.titulo}
+                                        description={elemento.descripcion}
+                                        url={("/noticias/" + elemento.id)}
+                                        NoticePage={elemento.fecha}
+                                        key={index}
+                                    />
+
+                            </li>
+                        )
+                    })}
+                </ul>
+                <Outlet />
+            </div>
+        )
+        }
     </>
 }
