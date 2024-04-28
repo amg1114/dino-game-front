5e0b879db0e1cdb2deecf33212cc7ece9cff9f3e
Felipe Cabal <phelipekabbal_85@hotmail.com>
2024-04-28 13:22:09 -0500

    solucion problema

--- src/pages/Noticias/VistaNoticia/VistaNoticia.jsx
+++ src/pages/Noticias/VistaNoticia/VistaNoticia.jsx
@@ -1,3 +1,50 @@
+import { Link } from "react-router-dom"
+import "../../../css/modal/index.css"
+import { useParams } from "react-router-dom"
+import axios from "axios"
+import { useEffect,  useState } from "react"
+
+
 export function VistaNoticia() {
-    return <h1>VistaNoticia</h1>
+
+    const { id } = useParams()
+    const ENDPOINT = `https://dinogame.up.railway.app/api/noticias/${id}`
+    const [noticia, setNoticia] = useState(null)
+
+    useEffect(() => {
+        axios.get(ENDPOINT)
+            .catch(function (error) {
+                console.log(error)
+            })
+            .then(function (response) {
+                setNoticia(response.data);
+            })
+    }
+
+    )
+
+    return <>
+        {
+            noticia === null ? <></> : (
+                <div className="modal-fade animate__animated animate__fadeIn">
+                    <div className="modal-content animate__animated animate__slideInDown">
+                        <div className="modal-header">
+                            <Link to="/juegos" className="modal-closer">
+                                <span className="material-symbols-outlined">
+                                    close
+                                </span>
+                            </Link>
+                        </div>
+                        <div className="izquierda">
+                            <img src={noticia.assets[0].url} alt="imagen_juego" />
+                            <h2>{noticia.titulo}</h2>
+                            <p>{noticia.descripcion}</p>
+                        </div>
+                        <div className="derecha">
+
+                        </div>
+                    </div>
+                </div>)
+        }
+    </>
 }
