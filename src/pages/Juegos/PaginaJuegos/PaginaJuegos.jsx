c0d69c157975ce6c7d0f995e138dca3379604c3f
Felipe Cabal <phelipekabbal_85@hotmail.com>
2024-04-30 12:33:11 -0500

    inicializacion de pagina juegos

--- src/pages/Juegos/PaginaJuegos/PaginaJuegos.jsx
+++ src/pages/Juegos/PaginaJuegos/PaginaJuegos.jsx
@@ -1,14 +1,46 @@
 import { Outlet } from "react-router-dom";
 import { FormularioFiltros } from "../../../partials/FormularioFiltros/FormularioFiltros";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { GameCard } from "../../../components/GameCard/GameCard";
 
 // juegos 
 // juegos/1
 export function PaginaJuegos() {
+    const ENDPOINT = process.env.REACT_APP_API + "/video-games"
+    const [juegos, setJuegos] = useState([])
+
+    useEffect(() => {
+        axios.get(ENDPOINT)
+            .catch(function (error) {
+                console.log(error)
+            })
+            .then(function (respuesta) {
+                setJuegos(respuesta.data)
+            })
+    })
+
     return <>
-        <div className="container">
-            <FormularioFiltros onSearch={(data)=>console.log({data})} />
-            <h1>Pagina Juegos</h1>
-            <Outlet />
-        </div>
+        {juegos === null ? <></> : (
+            <div>
+                <FormularioFiltros onSearch={(data) => console.log({ data })} />
+                <div className="lista-juegos">
+                    {
+                        juegos.map((juego, index) => {
+                            <div className="juego" key={index}>
+                                <GameCard
+                                    image={juego.assets[0].url}
+                                    title={juego.titulo}
+                                    description={juego.description}
+                                    url={"/video-game/" + juego.id}
+                                />
+                            </div>
+                        })
+                    }
+                </div>
+                <Outlet />
+            </div>
+        )
+        }
     </>
 }
