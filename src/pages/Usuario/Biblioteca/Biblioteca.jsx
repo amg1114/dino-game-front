d28f37acf9e682f6d5571830b0c7e20580f4103e
dancuest <102336853+dancuest@users.noreply.github.com>
2024-05-10 01:25:33 -0500

    adelanto de la biblioteca

--- src/pages/Usuario/Biblioteca/Biblioteca.jsx
+++ src/pages/Usuario/Biblioteca/Biblioteca.jsx
@@ -1,3 +1,29 @@
+import axios from "axios";
+import { useEffect, useState } from "react"
+import { GameCard } from "../../../components/GameCard/GameCard";
+
 export function Biblioteca(){
-    return <></>
+
+    const ENDPOINT_BBLIOTECA = "https://dinogame.up.railway.app/api/video-games/user"
+    const [biblioteca, setBiblioteca]=useState([]);
+
+    useEffect(()=>{
+        axios.get(ENDPOINT_BBLIOTECA)
+        .catch(function(error){
+            console.log(error);
+        }).then(function(respuesta){
+            setBiblioteca(respuesta.data.videoGame)
+        })
+    },[])
+
+    return <>{
+        biblioteca === null ? <><h2>El usuario no tiene juegos descargados</h2></> : (
+            <div className="lista-juegos">
+                <h2><span>DINO</span>BIBLIOTECA</h2>
+                {biblioteca.map((juego, index) => (
+                    <GameCard key={index} Game={juego} />
+                ))}
+            </div>
+        )
+    }</>
 }
