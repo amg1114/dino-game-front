2d5aac809a202d83b6e67d1f931cc1feb35bbeda
dancuest <102336853+dancuest@users.noreply.github.com>
2024-05-12 01:32:32 -0500

    completion of the library section

--- src/pages/Usuario/Biblioteca/Biblioteca.jsx
+++ src/pages/Usuario/Biblioteca/Biblioteca.jsx
@@ -1,27 +1,30 @@
 import axios from "axios";
 import { useEffect, useState } from "react"
 import { GameCard } from "../../../components/GameCard/GameCard";
+import { useAuth } from "../../../providers/AuthProvider";
 
-export function Biblioteca(){
+export function Biblioteca() {
+    const { usuario } = useAuth();
+    const ENDPOINT_BBLIOTECA = "https://dinogame.up.railway.app/api/video-games/user/"
+    const [biblioteca, setBiblioteca] = useState([]);
 
-    const ENDPOINT_BBLIOTECA = "https://dinogame.up.railway.app/api/video-games/user"
-    const [biblioteca, setBiblioteca]=useState([]);
-
-    useEffect(()=>{
-        axios.get(ENDPOINT_BBLIOTECA)
-        .catch(function(error){
-            console.log(error);
-        }).then(function(respuesta){
-            setBiblioteca(respuesta.data.videoGame)
-        })
-    },[])
+    useEffect(() => {
+        if (usuario) {
+            axios.get(ENDPOINT_BBLIOTECA + usuario.id)
+                .catch(function (error) {
+                    console.log(error);
+                }).then(function (respuesta) {
+                    setBiblioteca(respuesta.data)
+                })
+        }
+    }, [])
 
     return <>{
         biblioteca === null ? <><h2>El usuario no tiene juegos descargados</h2></> : (
             <div className="lista-juegos">
                 <h2><span>DINO</span>BIBLIOTECA</h2>
                 {biblioteca.map((juego, index) => (
-                    <GameCard key={index} Game={juego} />
+                    <GameCard key={index} Game={juego.videoGame} />
                 ))}
             </div>
         )
