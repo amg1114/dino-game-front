4a6c4265163ba525963873a1617a692cf6601d6c
1Nath00 <nataliagomezdelacruz1424@gmail.com>
2024-05-17 18:29:59 -0500

    Agregar informacion campos faltantes vista-juego

--- src/pages/Juegos/VistaJuego/VistaJuego.jsx
+++ src/pages/Juegos/VistaJuego/VistaJuego.jsx
@@ -39,7 +39,7 @@ export function VistaJuego() {
                             </div>
                             <div className="vista-juego">
                                 <div className="info-izquierda">
-                                    <div> <img className="img1" src={juego.assets[0].url} alt={juego.assets[0].titulo} /></div>
+                                    <div> <img className="img1" src={juego.assets[0].url} alt={juego.assets[0].title} /></div>
                                     <div>
                                         {juego.assets.slice(1, juego.assets.length).map((img, index) => {
                                             return (<img className="mas-img" key={index} src={img.url} alt={img.title} />)
@@ -48,9 +48,9 @@ export function VistaJuego() {
                                     </div>
                                     <div className="det-dev-size">
                                         <div>
-                                            <p>desarrollador <br /><span className="det-nom-edit">Nombre: <br /></span> <span className="det-nom-edit">Editor:</span></p>
+                                            <p>desarrollador <br /><span className="det-nom-edit">Nombre: {juego.developer.user.nombre}  <br /></span> <span className="det-nom-edit">Editor: {juego.developer.user.nombre} </span></p>
                                         </div>
-                                        <p className="size">tamaño</p>
+                                        <p className="size">tamaño: <span className = "requisitos">100GB</span></p>
                                     </div>
                                     <p className="comentarios">comentarios</p>
 
@@ -58,24 +58,35 @@ export function VistaJuego() {
                                 <div className="info-derecha">
                                     <h2 className="titulo-juego">{juego.titulo}</h2>
                                     <p> <span className="descripcion"> descripción <br /></span>  {juego.descripcion} </p>
-                                    
+
                                     <p className="cat">categoria</p>
 
                                     <div className="categoria">
                                         {juego.categorias.map((index) => {
-                                            return ( <h3 className="categoria-juego">{index.titulo}</h3> )
+                                            return (<h3 className="categoria-juego">{index.titulo}</h3>)
                                         })
                                         }
                                     </div>
 
                                     <p>
-                                        <span className="version">información de la versión reciente</span> <br />
+                                        <span className="info">información de la versión reciente
+
+                                            <span className="version">{juego.assets[0].title}</span> </span> <br />
                                     </p>
 
-                                    <p><span className="req">requisitos</span><br /> </p>
+                                    <p><span className="req">requisitos:  {juego.versions[0].requisitos.map((req, index) => {
+                                        return (<span className = "requisitos">{req.requisito} </span >)
+                                    })}</span><br /> </p>
+                                    
                                     <div className="valor-comprar">
-                                        <h2 className="precio">precio: col${juego.precio}</h2>
-                                        <button className="btn-comprar" type="button">comprar</button>
+                                        <div className="comprar">
+                                            <h2 className="precio">precio: col${juego.precio}</h2>
+                                            <h2 className="descuento">descuento: ${juego.descuento}</h2>
+                                            <h2 calssName="descuento">% descuento: ${juego.descuento}</h2>
+                                        </div>
+                                            <span><button className="btn-comprar" type="button">comprar</button></span>
+                                        
+                                       
                                     </div>
                                 </div>
                             </div>
