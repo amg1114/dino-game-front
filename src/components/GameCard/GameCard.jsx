6b7ca06d5b852112ef80519f5b61e0bdc50e6e1a
Alejandro Moreno <alejandromogil@gmail.com>
2024-05-28 19:56:05 -0500

    Refactor Categoria

--- src/components/GameCard/GameCard.jsx
+++ src/components/GameCard/GameCard.jsx
@@ -2,47 +2,43 @@ import axios from 'axios'
 import { Link } from 'react-router-dom'
 import './gameCard.css'
 import { useEffect, useState } from 'react'
+import { useAuth } from '../../providers/AuthProvider'
 
 export function GameCard({ Game }) {
     const { assets, titulo, descripcion, id, descuentos, precio } = Game
-
+    const { usuario } = useAuth()
     const ENDPOINT_API = `${process.env.REACT_APP_API}/video-games/biblioteca/${id}`
-    const [validacion, setValidacion] = useState(false);
+    const [isAdquired, setIsAdquired] = useState(false);
 
     useEffect(() => {
-        console.log(descuentos)
-        axios.get(ENDPOINT_API)
-            .then(function () {
-                setValidacion(true);
-            })
-    }, [validacion]
-    )
+        if (usuario) {
+            axios.get(ENDPOINT_API)
+                .then(function () {
+                    setIsAdquired(true);
+                })
+        }
+    }, [usuario]);
 
     return <>
 
         <div className='gameCardSection'>
-            {assets[0] ? <figure>
-                <img className="gameImage"
-                    src={assets[0].asset.url}
-                    alt={titulo + " imagen"}
-                />
-            </figure> : <></>}
-
+            { assets[0] ? 
+                <figure>
+                    <img className="gameImage"
+                        src={assets[0].asset.url}
+                        alt={titulo + " imagen"}
+                    />
+                </figure> : 
+                <></>
+            }
             <div className='description'><p className='description-texto'>{descripcion.slice(0, 100)}</p></div>
-
-            {validacion ? <><div className="urlGame">
-                <Link to={"/perfil/biblioteca"} className='stretched-link'>{titulo}</Link>
+            <div className="urlGame">
+                <Link to={isAdquired ? "/perfil/biblioteca" : "/juegos/" + id} className='stretched-link'>{titulo}</Link>
             </div>
-                <span className="btn btn-1 precio">ADQUIRIDO</span>
-            </> : <>
-                <div className="urlGame">
-                    <Link to={"/juegos/" + id} className='stretched-link'>{titulo}</Link>
-                </div>
-                {(descuentos === undefined || descuentos.length === 0) ?
-                    <span className="btn btn-1 precio">${precio}</span> :
-                    <span className="btn btn-1 precio">${precio} %{descuentos[0].porcentaje}</span>
-                }
-            </>}
+            {isAdquired ? <span className="btn btn-1 precio">ADQUIRIDO</span> : (descuentos === undefined || descuentos.length === 0) ?
+                <span className="btn btn-1 precio">${precio}</span> :
+                <span className="btn btn-1 precio">${precio} %{descuentos[0].porcentaje}</span>
+            }
         </div>
     </>
 }
