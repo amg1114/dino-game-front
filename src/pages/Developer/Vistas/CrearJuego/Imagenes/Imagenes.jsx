8ac399271efdad11322304825ecd4c468a57b9a1
Alejandro Moreno <alejandromogil@gmail.com>
2024-05-29 22:00:04 -0500

    Disable Unready assets form

--- src/pages/Developer/Vistas/CrearJuego/Imagenes/Imagenes.jsx
+++ src/pages/Developer/Vistas/CrearJuego/Imagenes/Imagenes.jsx
@@ -1,16 +1,16 @@
-import AssetsForm from "../../../../../components/assetsForm/AssetsForm"
+
 
 export function Imagenes({ assets, handleAssetChange, handleAssetDelete, handleAssetReset }) {
     return <>
         <div className="formulario-agregar-juego">
             <div className="contenedor-form-modal">
-                <AssetsForm
+                {/* <AssetsForm
                     assets={assets}
                     maxFiles={5}
                     onChange={(asset) => handleAssetChange(asset)}
                     onDelete={(id) => handleAssetDelete(id)}
                     onReset={() => handleAssetReset()}
-                />
+                /> */}
             </div>
         </div>
     </>
